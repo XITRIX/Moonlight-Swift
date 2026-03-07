9bbd35f9dbd459a47a68426bf3bf87d767448af2
XITRIX <xitrix@bk.ru>
2026-03-07 21:40:57 +0100

    Initial Commit

---
+++ Moonlight-Swift/Moonlight_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moonlight_SwiftApp.swift
+//  Moonlight-Swift
+//
+//  Created by Даниил Виноградов on 07.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Moonlight_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
